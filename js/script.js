0e400e17d053140f16af623ed5a21381d3f37632
valyafrankova22 <vfrankova22@icloud.com>
2021-11-14 00:16:04 +0200

    init

--- js/script.js
+++ js/script.js
@@ -26,38 +26,72 @@ console.log(lastIndexOf([1, 2, 3, 4], 2));
 
 
 // Task #3 Find
-function findFunc (array, value) {
-    for (let i of array) {
-        if (i === value) return value;
+const find =  (arr, callback) => {
+
+    for(let i = 0; i < arr.length; i++) {
+
+        if( callback( arr[i], i, arr )) {
+            return arr[i];
+        }
     }
-    return undefined;
 }
-console.log(findFunc([`Vlad`, `Valya`, `Anna`, `Kate`], `Valya`));
+console.log(find([`Vlad`, `Valya`, `Anna`, `Kate`], item => item === `Valya`));
 
 
 // Task #4 findIndex
-function findIndex (array, value) {
-    for (let i of array) {
-        if (i === value) return array.indexOf(value);
+const findIndex =  (arr, callback) => {
+
+    for(let i = 0; i < arr.length; i++) {
+
+        if( callback( arr[i], i, arr )) {
+            return i;
+        }
     }
-    return -1;
 }
-console.log(findIndex([`Vlad`, `Valya`, `Anna`, `Kate`], `Anna`));
-console.log(findIndex([`Vlad`, `Valya`, `Anna`, `Kate`], `Pasha `));
+console.log(findIndex([`Vlad`, `Valya`, `Anna`, `Kate`], item => item === `Kate`));
 
 
 // Task #5 Includes
-function includes(array, value) {
-    for(let i of array) {
-        if (i === value) return true;
+function includes (array, value) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === value) {
+            return true;
+        }
     }
     return false;
 }
-console.log(includes([1, 2, 3, 4, 5, 6], 4));
-console.log(includes([1, 2, 3, 4, 5, 6], 9));
+console.log(includes([1, 2, 3, 4], 2));
+console.log(includes([1, 2, 3, 4], 8));
+
 
 // Task #6 every
+const every = (array, callback) => {
+
+    for (let i = 0; i < array.length; i++) {
+
+        if(!callback(array[i], i, array)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(every([1, 3, 7, 5, 8], item => item < 10));
+console.log(every([1, 3, 7, 5, 8], item => item < 7));
+
 
 // Task #7 some
+const some = (array, callback) => {
 
+    for (let i = 0; i < array.length; i++) {
+
+        if(callback(array[i], i, array)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(some([1, 3, 7, 5, 8], item => item === 7));
+console.log(some([1, 3, 7, 5, 8], item => item === 4));
 
